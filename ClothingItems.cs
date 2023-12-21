0a5544c041d58ef8c117800b4411ea98e4947f28
P091913 <149612419+P091913@users.noreply.github.com>
2023-12-21 11:22:01 -0600

    Add files via upload
    Initial Upload

---
+++ ClothingItems.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorNavigatorManagerExample
+{
+
+    public class ObjectService
+    {
+        public ClothingItems MyInfo { get; set; }
+    }
+
+    public class ClothingItems
+    {
+        public string username { get; set; }  
+        
+        public string ItemName { get; set; }
+    }
+}
